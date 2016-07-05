e8b539e618c8fe960cda06d216e7bd1a44ae933a
Sid <siddhata.s.patil@gmail.com>
2016-07-05 11:22:11 -0700

    Create qs3.cpp

---
+++ qs3.cpp
@@ -0,0 +1,30 @@
+/*
+ ==============================================================================================================================
+ Name        : qs3.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Write a program that computes a running sum of inputs from the user, terminating when the
+               user gives an input value of 0
+ ==============================================================================================================================
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int no = 1;
+	int sum = 0;
+	while ( no != 0)
+	{
+		cin>>no;
+		sum = sum+no;
+		cout<<"Sum = "<< sum<< endl;
+	}
+system("pause");
+return 0;	
+}
+
